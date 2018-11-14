6ef63e07265f4981cad2addc6067e7a1e19a888e
Ronaldo Franco <ronaldojffranco72@gmail.com>
2018-11-13 22:16:42 -0200

    update

--- Frontend/js/services/fitGameService.js
+++ Frontend/js/services/fitGameService.js
@@ -4,8 +4,29 @@ angular.module("Fitgame")
 ApiCall.$inject = ['$http'];
 
 function ApiCall ($http){
-  
-  this.login = () => {
-    console.log( "teste");
+
+  this.login = (dataLogin) =>
+  {
+    return $http.post(`localhost/route`,dataLogin);
+  }
+  this.createRoom = (dataRoom) =>
+  {
+    return $http.post(`localhost/route`,dataRoom);
+  }
+  this.registerAccount = (dataNewUser) =>
+  {
+    return $http.post(`localhost/route`,dataNewUser);
+  }
+  this.createActivity = (dataNewActivity) =>
+  {
+    return $http.post(`localhost/route`,dataNewActivity);
+  }
+  this.getAllActivitys = () =>
+  {
+    return $http.get(`localhost/route?`);
+  }
+  this.getAllRooms = () =>
+  {
+    return $http.get(`localhost/route?`);
   }
 }
